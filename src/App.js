70302c85e48aa93b253299959a06da39fe57ffd6
rahulmistry751 <56232543+rahulmistry751@users.noreply.github.com>
2021-04-17 20:08:17 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import "./styles.css";
+const movieDb = {
+  Action: [
+    { name: "Tenet", rating: "5/5" },
+    { name: "Avengers: Endgame", rating: "5/5" },
+    { name: "Spider-Man: Far from Home", rating: "4.5/5" }
+  ],
+
+  Horror: [
+    { name: "The Haunting of Hill House", rating: "5/5" },
+    {
+      name: "The Haunting of Bly Manor",
+      rating: "4.5/5"
+    },
+    {
+      name: "Nun",
+      rating: "4/5"
+    }
+  ],
+  ScienceFiction: [
+    {
+      name: "Avengers: Endgame",
+      rating: "5/5"
+    },
+    {
+      name: "Ready Player One",
+      rating: "4/5"
+    },
+    {
+      name: "Passengers",
+      rating: "4/5"
+    }
+  ]
+};
+var lstDb = Object.keys(movieDb);
+export default function App() {
+  var [movies, setMovies] = useState("Action");
+  function showMovies(genre) {
+    console.log(genre);
+    setMovies(genre);
+    console.log("hi", movies);
+  }
+  return (
+    <div className="App">
+      <header>
+        <h1>
+          Moviflix <span role="img">🎬</span>
+        </h1>
+        <br />
+        <h3>Explore Movies and TV shows</h3>
+      </header>
+      <main>
+        <div className="genre">
+          {lstDb.map((item) => {
+            return <span onClick={() => showMovies(item)}>{item}</span>;
+          })}
+        </div>
+        {movieDb[movies].map((item) => (
+          <div className="movieList" key={item.name}>
+            <div style={{ fontSize: "larger" }}>{item.name}</div>
+            <div>Rating: {item.rating}</div>
+          </div>
+        ))}
+      </main>
+    </div>
+  );
+}
